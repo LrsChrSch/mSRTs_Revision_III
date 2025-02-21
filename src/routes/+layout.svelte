76337879863a38e022912a407d38e3909ba299db
lrs-chr-sch <86013790+LrsChrSch@users.noreply.github.com>
2025-02-21 17:02:22 +0100

    Remove unused files and update Svelte adapter to static for deployment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	// The layout only sets up the main page (this website is technically a one-pager)
 	// Mainly the css gets loaded here
 	let { children } = $props();
+
 	import '@fontsource-variable/stix-two-text';
 	import '../app.css';
 </script>
